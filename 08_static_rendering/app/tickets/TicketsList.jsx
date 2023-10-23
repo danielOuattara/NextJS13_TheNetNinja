832082ffec8255ce85dc04bf08851aa7872d21d2
danielOuattara <daniel.ouattara@gmx.com>
2023-10-23 22:42:46 +0200

    08 static rendering

---
+++ 08_static_rendering/app/tickets/TicketsList.jsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+async function fetchTickets() {
+  const response = await fetch("http://localhost:4000/tickets", {
+    next: { revalidate: 0 },
+  });
+  return response.json();
+}
+
+export default async function TicketsList() {
+  const tickets = await fetchTickets();
+  return (
+    <>
+      {tickets.length === 0 && (
+        <p className="text-center">There is non open tickets</p>
+      )}
+
+      {tickets.map((ticket) => (
+        <div key={ticket.id} className="card my-5">
+          <Link href={`/tickets/${ticket.id}`}>
+            <h3>{ticket.title}</h3>
+            <p>{ticket.body.slice(0, 150)}...</p>
+            <div className={`pill ${ticket.priority}`}>
+              {ticket.priority} priority
+            </div>
+          </Link>
+        </div>
+      ))}
+    </>
+  );
+}
---
+++ 08_static_rendering/app/tickets/page.jsx
@@ -0,0 +1,17 @@
+import TicketsList from "./TicketsList";
+
+export default function Tickets() {
+  return (
+    <main>
+      <nav>
+        <div>
+          <h2>Tickets page</h2>
+          <p>
+            <small>Currently open tickets.</small>
+          </p>
+        </div>
+      </nav>
+      <TicketsList />
+    </main>
+  );
+}
