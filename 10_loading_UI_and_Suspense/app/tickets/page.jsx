130f5eaaec5ad8237b9468695c11f3f6f7c99315
danielOuattara <daniel.ouattara@gmx.com>
2023-10-23 22:43:17 +0200

    10 loading UI & Suspense

---
+++ 10_loading_UI_and_Suspense/app/tickets/page.jsx
@@ -0,0 +1,21 @@
+import { Suspense } from "react";
+import TicketsList from "./TicketsList";
+import Loading from "../loading";
+
+export default function Tickets() {
+  return (
+    <main>
+      <nav>
+        <div>
+          <h2>Tickets page</h2>
+          <p>
+            <small>Currently open tickets.</small>
+          </p>
+        </div>
+      </nav>
+      <Suspense fallback={<Loading />}>
+        <TicketsList />
+      </Suspense>
+    </main>
+  );
+}
