28c8d6ab5f6ea9781a3cb57a8cf061c4d7171643
danielOuattara <daniel.ouattara@gmx.com>
2023-10-23 22:42:32 +0200

    07 dynamic segments params

---
+++ 07_dynamic_segments_params/app/tickets/[ticketId]/page.jsx
@@ -0,0 +1,26 @@
+async function fetchSingleTicket(id) {
+  const response = await fetch(`http://localhost:4000/tickets/${id}`, {
+    next: { revalidate: 10 },
+  });
+  return response.json();
+}
+
+export default async function TicketDetails(props) {
+  const ticket = await fetchSingleTicket(props.params.ticketId);
+
+  return (
+    <main>
+      <nav>
+        <h2>Ticket Details</h2>
+      </nav>
+      <div className="card">
+        <h3>{ticket.title}</h3>
+        <small>Created by {ticket.user_email}</small>
+        <p>{ticket.body}</p>
+        <div className={`pill ${ticket.priority}`}>
+          {ticket.priority} priority
+        </div>
+      </div>
+    </main>
+  );
+}
