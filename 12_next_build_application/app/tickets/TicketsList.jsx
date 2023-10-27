0702f181429e6e679907a03c729bdd90175cfd66
danielOuattara <daniel.ouattara@gmx.com>
2023-10-27 23:30:44 +0200

    12 Next Build application

---
+++ 12_next_build_application/app/tickets/TicketsList.jsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+
+async function fetchTickets() {
+  // simulate delay
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  //---
+  const response = await fetch("http://localhost:4000/tickets", {
+    next: { revalidate: 0 },
+  });
+
+  return response.json();
+}
+
+export default async function TicketsList() {
+  const tickets = await fetchTickets();
+
+  if (tickets.length === 0) {
+    return <p className="text-center">There is non open tickets</p>;
+  }
+
+  return (
+    <>
+      {tickets.map((ticket) => (
+        <div key={ticket.id} className="card my-5">
+          <Link href={`/tickets/${ticket.id}`}>
+            <h3>{ticket.title}</h3>
+            <p>{ticket.body.slice(0, 150)}...</p>
+            <div className={`pill ${ticket.priority}`}>
+              {ticket.priority} priority
+            </div>
+          </Link>
+        </div>
+      ))}
+    </>
+  );
+}
